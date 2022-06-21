762e2a009bc3d387e4a99628e3acd53e3d58ee5c
ksw4811 <83329956+ksw4811@users.noreply.github.com>
2022-06-21 21:07:41 +0900

    Add files via upload
    6/21 
    
    1. AI 캐릭터를 만들기 위해서 클래스를 새로 만듬
    2. AI 컨트롤러를 만들어서 AI 캐릭터에 넣어 블랙보드, 비헤이비어 트리 연결해서 사용할 예정

---
+++ Source/ZombieShooting/MyAICharacter.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAICharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFrameWork/CharacterMovementComponent.h"
+#include "Animation/AnimInstance.h"
+
+// Sets default values
+AMyAICharacter::AMyAICharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_EASYMODEL(TEXT("/Game/File/Mesh/Grux.Grux"));
+	if (SK_EASYMODEL.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SK_EASYMODEL.Object);
+	}
+
+	GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance> PLAYER_ANIM(TEXT("/Game/BluePrints/Grux_AnimBlueprint.Grux_AnimBlueprint_C"));
+	if (PLAYER_ANIM.Succeeded())
+	{
+		GetMesh()->SetAnimInstanceClass(PLAYER_ANIM.Class);
+	}
+
+	GetCharacterMovement()->JumpZVelocity = 400.0f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+
+	GetCapsuleComponent()->SetCollisionProfileName(TEXT("MyAI"));
+}
+
+// Called when the game starts or when spawned
+void AMyAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyAICharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
--- Source/ZombieShooting/ZombieShootingProjectile.cpp
+++ Source/ZombieShooting/ZombieShootingProjectile.cpp
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+	// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ZombieShootingProjectile.h"
 #include "GameFramework/ProjectileMovementComponent.h"
